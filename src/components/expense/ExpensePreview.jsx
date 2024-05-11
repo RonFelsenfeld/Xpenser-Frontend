1ccff44bcaae8527ad8782741f789551c36a5a32
Ron Felsenfeld <ronfelsenfeld@gmail.com>
2024-05-11 21:35:32 +0300

    Improve expense preview style and layout

--- src/components/expense/ExpensePreview.jsx
+++ src/components/expense/ExpensePreview.jsx
@@ -1,14 +1,45 @@
 import { Link } from 'react-router-dom'
 
 export function ExpensePreview({ expense, onRemoveExpense }) {
-  return (
-    <article className="expense-preview flex align-center  justify-between">
-      <p className="expense-txt">
-        {expense.txt} - <span className="expense-amount">{expense.amount}</span>
-      </p>
+  function getCapitalizedCategory(category) {
+    return category.charAt(0).toUpperCase() + category.slice(1)
+  }
 
-      <p>{expense.category}</p>
-      <p>{expense.at}</p>
+  function formatAmountToCurrency(amount) {
+    const formattedAmount = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'ILS',
+    }).format(amount)
+
+    return formattedAmount
+  }
+
+  const { txt, amount, category, at } = expense
+  return (
+    <article className="expense-preview flex align-center justify-between">
+      <p className="bold-detail expense-txt">{txt}</p>
+      <p className="bold-detail expense-amount">{formatAmountToCurrency(amount)}</p>
+
+      {category ? (
+        <p
+          className="expense-category"
+          style={{ backgroundColor: `var(--category-${category}-clr)` }}
+        >
+          {getCapitalizedCategory(category)}
+        </p>
+      ) : (
+        <Link to={`/expense/edit/${expense._id}`} className="btn-no no-category">
+          <button>- Add category</button>
+        </Link>
+      )}
+
+      {at ? (
+        <p className="expense-at">{at}</p>
+      ) : (
+        <Link to={`/expense/edit/${expense._id}`} className="btn-no no-date">
+          <button>- Add date</button>
+        </Link>
+      )}
 
       <div className="actions-container flex align-center">
         <Link to={`/expense/edit/${expense._id}`}>
