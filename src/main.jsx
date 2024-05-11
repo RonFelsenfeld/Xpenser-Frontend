98911e6e455f847a839f0a1af67c043b7cc0c0f0
Ron Felsenfeld <ronfelsenfeld@gmail.com>
2024-05-11 16:34:20 +0300

    Init repo

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { App } from './RootCmp.jsx'
+import './assets/style/main.scss'
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
